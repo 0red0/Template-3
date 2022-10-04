3b7fe4e21696af81c842f50f1c4c5c22e4403cba
yhyawy <yhyawy1@gmail.com>
2022-10-04 23:13:50 +0200

    working on time up function

--- temp3.js
+++ temp3.js
@@ -1,20 +1,45 @@
-const skillsSection = document.querySelector(".skills");
-const bars = document.querySelectorAll(".skills .bars span");
+// const skillsSection = document.querySelector(".skills");
+// const bars = document.querySelectorAll(".skills .bars span");
 
-// To Top Button
-let topPg = document.querySelector(".top-pg");
-window.onscroll = function () {
-   if (window.scrollY >= 600) {
-      topPg.style.display = "block";
-   } else {
-      topPg.style.display = "none";
+// // To-Top Button
+// let topPg = document.querySelector(".top-pg");
+// window.addEventListener("scroll", () => {
+//    if (window.scrollY >= 600) {
+//       topPg.style.display = "block";
+//    } else {
+//       topPg.style.display = "none";
+//    }
+// });
+
+// // Skill Bars fill on scroll
+// window.addEventListener("scroll", () => {
+//    if (window.scrollY >= skillsSection.offsetTop) {
+//       bars.forEach((bar) => {
+//          bar.style.width = bar.dataset.size;
+//       });
+//    }
+// });
+
+// Stats update on scroll
+const statsSection = document.querySelector(".stats");
+const nums = document.querySelectorAll(".stats .num");
+let upTime = false;
+
+window.addEventListener("scroll", () => {
+   if (window.scrollY >= statsSection.offsetTop) {
+      if (!upTime) {
+         nums.forEach((num) => countUp(num));
+      }
+      upTime = true;
    }
-};
-// Skill Bars fill on Section scroll
-window.onscroll = function () {
-   if (window.scrollY >= skillsSection.offsetTop) {
-      bars.forEach((bar) => {
-         bar.style.width = bar.dataset.size;
-      });
-   }
-};
+});
+//
+function countUp(el) {
+   let target = el.dataset.number;
+   let Icounter = setInterval(() => {
+      el.textContent++;
+      if (el.textContent >= target) {
+         clearInterval(Icounter);
+      }
+   }, 10);
+}
